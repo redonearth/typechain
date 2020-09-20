ca2327cc181b22b301e4adc6256cea6694895974
Redonearth <wona23@gmail.com>
2020-09-20 12:32:08 +0900

    #0.5 Types in Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Redonearth", age = 33, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, and you are ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, and you are ${gender}!`;
+};
+
+console.log(sayHi("Redonearth", 33, "male"));
+
+export {};
